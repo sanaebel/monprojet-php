02609d1b757ccd9b0854ebfa22933298f5d08d93
Sanae Belcadi <sanaebelcadi77@gmail.com>
2026-03-30 19:00:53 +0100

    Premier commit : projet PHP initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Bonjour, ceci est mon projet PHP !";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Mon Premier PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Bonjour, ceci est mon premier projet PHP !</h1>";
+    ?>
+</body>
+</html>
